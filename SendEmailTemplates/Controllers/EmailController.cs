2c68eaf11fe291d1287dc261a7eed864ab04c0d9
GiovanniAndrex <giovanniandrex@gmail.com>
2025-03-07 13:44:44 -0500

    Agregar archivos de proyecto.

---
+++ SendEmailTemplates/Controllers/EmailController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SendEmailTemplates.Services;
+
+namespace SendEmailTemplates.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmailController(IEmailService emailService) : ControllerBase
+    {
+        private readonly IEmailService _emailService = emailService;
+
+        [HttpPost]
+        public async Task<IActionResult> SendEmail(string to, string subject, string body)
+        {
+            await _emailService.SendEmailAsync(to, subject, body);
+            return Ok();
+        }
+    }
+}
---
+++ SendEmailTemplates/Services/EmailService.cs
@@ -0,0 +1,32 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace SendEmailTemplates.Services
+{
+    public interface IEmailService
+    {
+        Task SendEmailAsync(string to, string subject, string body);
+    }
+
+    public class EmailService(IConfiguration configuration) : IEmailService
+    {
+        private readonly IConfiguration _configuration = configuration;
+
+        public async Task SendEmailAsync(string to, string subject, string body)
+        {
+            var emailEmisor = _configuration.GetValue<string>("Config_Emails:Email");
+            var password = _configuration.GetValue<string>("Config_Emails:Password");
+            var host = _configuration.GetValue<string>("Config_Emails:Host");
+            var port = _configuration.GetValue<int>("Config_Emails:Port");
+
+            var smtpClient = new SmtpClient(host, port);
+            smtpClient.EnableSsl = true;
+            smtpClient.UseDefaultCredentials = false;
+
+            smtpClient.Credentials = new NetworkCredential(emailEmisor, password);
+            var message = new MailMessage(emailEmisor!, to, subject, body);
+
+            await smtpClient.SendMailAsync(message);
+        }
+    }
+}
